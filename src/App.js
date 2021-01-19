8e7963a453f2963bfb319fb6871d3cd0d1a9d7dd
kader-dev <47367098+kader-mb@users.noreply.github.com>
2021-01-19 15:02:41 +0100

    fetch movies using TMDB API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import requests from "./requests";
+import Row from "./Row";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> helloooooo</h1>
-      </header>
+      <Row
+        title="Netflix Originals"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending now" fetchURL={requests.fetchTrending} />
+      <Row title="Top rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Romance movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Horror movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
